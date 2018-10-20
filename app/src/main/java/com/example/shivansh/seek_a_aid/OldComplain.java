90cdfae052de04ac1a145ed64f5f964f874a60c9
Shivansh <iit2017097@iiita.ac.in>
2018-10-20 16:48:18 +0530

    Comp existing changes

--- app/src/main/java/com/example/shivansh/seek_a_aid/OldComplain.java
+++ app/src/main/java/com/example/shivansh/seek_a_aid/OldComplain.java
@@ -1,13 +1,37 @@
 package com.example.shivansh.seek_a_aid;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class OldComplain extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -28,6 +52,82 @@ public class OldComplain extends AppCompatActivity {
             list.add(values[i]);
         }
 
+        TextView allot_blink = findViewById(R.id.text_please_allot);
+        ObjectAnimator anim = ObjectAnimator.ofInt(allot_blink, "backgroundColor", Color.WHITE, Color.RED,
+                Color.WHITE);
+        anim.setDuration(1500);
+        anim.setEvaluator(new ArgbEvaluator());
+        anim.setRepeatMode(ValueAnimator.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.start();
+
+        allot_blink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Dialog dialog = new Dialog(OldComplain.this);
+                dialog.setContentView(R.layout.allot_dialog);
+                dialog.setTitle("Please Allot Members");
+                Button dialogButton = dialog.findViewById(R.id.submit_allotment);
+                // if button is clicked, close the custom dialog
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText name1_t = dialog.findViewById(R.id.member_1_roll);
+                        EditText name2_t = dialog.findViewById(R.id.member_2_roll);
+                        String name1 = name1_t.getText().toString();
+                        String name2 = name2_t.getText().toString();
+                        Log.e("loghidgigis",name1+""+name2);
+                        String result;
+                        verifyGymkhana verify = new verifyGymkhana();
+                        try {
+                           result = verify.execute(name1,name2).get();
+                           Log.e("log",result);
+                           Toast.makeText(OldComplain.this,result,Toast.LENGTH_LONG).show();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+        TextView update_status = findViewById(R.id.text_update_status);
+        update_status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = getLayoutInflater();
+                View alertLayout = inflater.inflate(R.layout.update_dialog, null);
+                AlertDialog.Builder alert = new AlertDialog.Builder(OldComplain.this);
+                alert.setTitle("Update Description");
+                alert.setView(alertLayout);
+                // disallow cancel of AlertDialog on click of back button and outside touch
+                alert.setCancelable(false);
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                       // String user = etUsername.getText().toString();
+                       // String pass = etEmail.getText().toString();
+                        Toast.makeText(getBaseContext(), "Username: " +  " Email: " , Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog dialog = alert.create();
+                dialog.show();
+            }
+        });
+
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
@@ -35,5 +135,85 @@ public class OldComplain extends AppCompatActivity {
         mAdapter = new MyAdapter(list);
         recyclerView.setAdapter(mAdapter);
 
+    }
+    private static class verifyGymkhana extends AsyncTask<String, Void, String> {
+
+        static final String REQUEST_METHOD = "GET";
+        public static final int READ_TIMEOUT = 15000;
+        public static final int CONNECTION_TIMEOUT = 15000;
+
+        protected String doInBackground(String... userdata) {
+
+            String name1 = userdata[0];
+            String name2 = userdata[1];
+            String inputLine;
+            String result=null;
+            //https://prototype-swastik0310.c9users.io/<name>/<email>/<type>
+            String base_url = "https://prototype-swastik0310.c9users.io/alot/volunteer/"+name1+"/"+name2;
+            Log.e("log","Url : "+base_url);
+
+            try {
+                URL myUrl = new URL(base_url);
+
+                //Create a connection
+                HttpURLConnection connection =(HttpURLConnection) myUrl.openConnection();
+
+                connection.setRequestMethod(REQUEST_METHOD);
+                connection.setReadTimeout(READ_TIMEOUT);
+                connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+                //Connect to our url
+                connection.connect();
+
+                InputStreamReader streamReader = new
+                        InputStreamReader(connection.getInputStream());
+                //Create a new buffered reader and String Builder
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                //Check if the line we are reading is not null
+                while((inputLine = reader.readLine()) != null){
+                    stringBuilder.append(inputLine);
+                }
+                //Close our InputStream and Buffered reader
+                reader.close();
+                streamReader.close();
+                //Set our result equal to our stringBuilder
+                result = stringBuilder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_complete:
+                if (checked)
+                    Toast.makeText(OldComplain.this,"Complete",Toast.LENGTH_SHORT).show();
+                    break;
+            case R.id.radio_in_progress:
+                if (checked)
+                    Toast.makeText(OldComplain.this,"In Progress",Toast.LENGTH_SHORT).show();
+                    break;
+            case R.id.radio_not_possible:
+                if(checked)
+                    Toast.makeText(OldComplain.this,"Not Possible",Toast.LENGTH_SHORT).show();
+                    break;
+        }
     }
 }
